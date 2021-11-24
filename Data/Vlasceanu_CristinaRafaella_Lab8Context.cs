9d6e0035e536ca2580b2011f5e432831a00f7b6f
Cristina Rafaella Vlasceanu <rafa.vlasceanu@yahoo.com>
2021-11-24 21:40:56 +0200

    Add project files.

---
+++ Data/Vlasceanu_CristinaRafaella_Lab8Context.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Vlasceanu_CristinaRafaella_Lab8.Models;
+
+namespace Vlasceanu_CristinaRafaella_Lab8.Data
+{
+    public class Vlasceanu_CristinaRafaella_Lab8Context : DbContext
+    {
+        public Vlasceanu_CristinaRafaella_Lab8Context (DbContextOptions<Vlasceanu_CristinaRafaella_Lab8Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Vlasceanu_CristinaRafaella_Lab8.Models.Book> Book { get; set; }
+
+        public DbSet<Vlasceanu_CristinaRafaella_Lab8.Models.Publisher> Publisher { get; set; }
+
+        public DbSet<Vlasceanu_CristinaRafaella_Lab8.Models.Category> Category { get; set; }
+    }
+}
